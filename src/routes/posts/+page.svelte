18cde40d4c850d5330f03fb0c6b8d31321e36049
Baby Feavel <feavel@qq.com>
2023-08-12 18:58:25 +0800

    cool

--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -1,41 +1,15 @@
 <script lang="ts">
-	import { Autocomplete, type AutocompleteOption } from '@skeletonlabs/skeleton';
-
 	let inputDemo = '';
-
-	const flavorOptions: AutocompleteOption[] = [
-		{ label: 'Vanilla', value: 'vanilla', keywords: 'plain, basic', meta: { healthy: false } },
-		{ label: 'Chocolate', value: 'chocolate', keywords: 'dark, white', meta: { healthy: false } },
-		{ label: 'Strawberry', value: 'strawberry', keywords: 'fruit', meta: { healthy: true } },
-		{
-			label: 'Neapolitan',
-			value: 'neapolitan',
-			keywords: 'mix, strawberry, chocolate, vanilla',
-			meta: { healthy: false }
-		},
-		{ label: 'Pineapple', value: 'pineapple', keywords: 'fruit', meta: { healthy: true } },
-		{ label: 'Peach', value: 'peach', keywords: 'fruit', meta: { healthy: true } }
-	];
-	function onFlavorSelection(event: any): void {
-		inputDemo = event.detail.label;
-	}
 </script>
 
 <div class="flex justify-between flex-col-reverse md:flex-row">
 	<div class="flex flex-col gap-4 h-1/2">
-		<button class="btn variant-filled">I also want to write ✍️!</button>
+		<a href="/posts/create-post" class="btn variant-filled">I also want to write ✍️!</a>
 		<div class="input-group input-group-divider grid-cols-[auto_1fr_auto] border-0">
 			<div class="input-group-shim">/posts/</div>
 			<input type="search" name="demo" bind:value={inputDemo} placeholder="Search..." />
 			<button class="variant-filled-secondary">Search</button>
 		</div>
-		<!-- <div class="card w-full max-w-sm max-h-48 p-4 overflow-y-auto" tabindex="-1">
-			<Autocomplete
-				bind:input={inputDemo}
-				options={flavorOptions}
-				on:selection={onFlavorSelection}
-			/>
-		</div> -->
 	</div>
 	<div class="text-right">
 		<p class="text-xs italic text-gray-500 py-2">
